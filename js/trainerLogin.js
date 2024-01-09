618c9a90f543090ae325dd6113f82d3138eec87d
OshandaB <oshanda893@gmail.com>
2024-01-09 15:39:55 +0530

    redirect dashboard

--- js/trainerLogin.js
+++ js/trainerLogin.js
@@ -18,13 +18,8 @@ $('#btnLogin').click(function () {
         data: JSON.stringify({ "email": email, "password": password }),  // Convert data to JSON string
         success: function(response) {
             console.log(response);
-            // Check if the login was successful (adjust this based on your API response)
-            if (response.success) {
-                // Redirect to the trainer dashboard
-                window.location.href = '../pages/trainer/index.html';  // Adjust the URL accordingly
-            } else {
-                console.error('Login failed. Handle the error case.');
-            }
+            window.location.href = '../trainer/index.html';
+
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.error(jqXHR.responseText);  // Log the response text for debugging
