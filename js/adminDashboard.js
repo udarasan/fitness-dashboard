dfd3436e1de247f5f6853536501edbdf3fe29fa0
Ksandeepa07 <kaveensandeepa512@gmail.com>
2024-01-09 21:15:01 +0530

    design admin dashboard

---
+++ js/adminDashboard.js
@@ -0,0 +1,29 @@
+var dashboard = document.getElementById('dashboard');
+var trainer = document.getElementById('trainer');
+var member = document.getElementById('member');
+
+
+document.getElementById("dashboardTab").addEventListener('click', function () {
+    trainer.style.display='none '
+    member.style.display='none '
+    dashboard.style.display = 'block';
+
+})
+
+
+document.getElementById("trainerTab").addEventListener('click', function () {
+    dashboard.style.display = 'none';
+    member.style.display='none '
+    trainer.style.display='block '
+
+})
+
+
+document.getElementById("memberTab").addEventListener('click', function () {
+    dashboard.style.display = 'none';
+    trainer.style.display='none '
+    member.style.display='block '
+
+})
+
+
