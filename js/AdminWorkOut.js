eb19284e6c9dfc4072444a38d8fbb816551432e5
duvindu111 <duvinduthathsara@gmail.com>
2024-01-14 16:44:02 +0530

    modified modalAssignBtn in AdminWorkout.js

--- js/AdminWorkOut.js
+++ js/AdminWorkOut.js
@@ -255,11 +255,11 @@ $("#modalAssignBtn").click(function(){
     let userId = $("#memberSelect").val();
     // Make the AJAX request
     $.ajax({
-        url: 'http://localhost:8080/api/v1/user/updateUser',
+        url: 'http://localhost:8080/api/v1/user/update',
         method: 'POST',
         dataType: 'json',
         contentType: 'application/json',  // Set content type to JSON
-        data: JSON.stringify({"email": currUserEmail, "workout_id": workoutId}),  // Convert data to JSON string
+        data: JSON.stringify({"email": currUserEmail, "workout_id": workoutId, "uid": userId}),  // Convert data to JSON string
         success: function (response) {
             console.log(response);
             $('#assignWorkoutModal').data('bs.modal').hide();
