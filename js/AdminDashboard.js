69e2480254cd9c46d0ec57a01de38a09a59b7db0
duvindu111 <duvinduthathsara@gmail.com>
2024-01-18 23:13:54 +0530

    got data to 4 cards in admin dashboard

--- js/AdminDashboard.js
+++ js/AdminDashboard.js
@@ -1,29 +1,82 @@
-// var dashboard = document.getElementById('dashboard');
-// var trainer = document.getElementById('trainer');
-// var member = document.getElementById('member');
-//
-//
-// document.getElementById("dashboardTab").addEventListener('click', function () {
-//     trainer.style.display='none '
-//     member.style.display='none '
-//     dashboard.style.display = 'block';
-//
-// })
-//
-//
-// document.getElementById("trainerTab").addEventListener('click', function () {
-//     dashboard.style.display = 'none';
-//     member.style.display='none '
-//     trainer.style.display='block '
-//
-// })
-//
-//
-// document.getElementById("memberTab").addEventListener('click', function () {
-//     dashboard.style.display = 'none';
-//     trainer.style.display='none '
-//     member.style.display='block '
-//
-// })
-//
-//
+$(document).ready(function () {
+    getTrainerCount();
+});
+
+function getTrainerCount(){
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/getTrainerCount',
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+            trainerCount = response.data;
+            $("#trainersCount").text(trainerCount);
+            getMemberCount();
+        },
+        error: function (jqXHR) {
+            console.log(jqXHR.responseText);
+        }
+    })
+}
+
+function getMemberCount(){
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/getMemberCount',
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+            memberCount = response.data;
+            $("#membersCount").text(memberCount);
+            getWorkoutPlansCount();
+        },
+        error: function (jqXHR) {
+            console.log(jqXHR.responseText);
+        }
+    })
+}
+
+function getWorkoutPlansCount(){
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/getWorkoutPlanCount',
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+            workoutCount = response.data;
+
+            if(workoutCount==1){
+                $("#workoutCount").text(workoutCount);
+                $("#lblWorkoutCount").text("Workout Plan");
+            }else{
+                $("#workoutCount").text(workoutCount);
+            }
+            getMealPlansCount();
+        },
+        error: function (jqXHR) {
+            console.log(jqXHR.responseText);
+        }
+    })
+}
+
+function getMealPlansCount(){
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/getMealPlanCount',
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+            mealCount = response.data;
+
+            if(mealCount==1){
+                $("#mealCount").text(mealCount);
+                $("#lblMealCount").text("Meal Plan");
+            }else{
+                $("#mealCount").text(workoutCount);
+            }
+        },
+        error: function (jqXHR) {
+            console.log(jqXHR.responseText);
+        }
+    })
+}
