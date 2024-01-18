8fa3179c6d7cc18bdd1bc5b01a802161779ce085
OshandaB <oshanda893@gmail.com>
2024-01-18 09:52:43 +0530

    login validaton added

--- js/TrainerLogin.js
+++ js/TrainerLogin.js
@@ -11,18 +11,28 @@ $('#btnLogin').click(function () {
         method: 'POST',
         dataType: 'json',
         contentType: 'application/json',  // Set content type to JSON
-        data: JSON.stringify({ "email": email, "password": password }),  // Convert data to JSON string
-        success: function(response) {
+        data: JSON.stringify({"email": email, "password": password}),  // Convert data to JSON string
+        success: function (response) {
             console.log(response);
             alert("Login Successful!");
             localStorage.setItem("trainer-email", email);
             window.location.href = '../trainer/index.html';
 
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             alert("Login Failed! Please check your credentials. Also, there might be an issue with the server.");
 
             console.error(jqXHR.responseText);  // Log the response text for debugging
         }
     });
+});
+$('#email').on('input', function () {
+    let email = $(this).val();
+
+    // Validate email
+    if (!isValidEmail(email)) {
+        $('#emailErrorLabel').text("Please enter a valid email address.");
+    } else {
+        $('#emailErrorLabel').text(""); // Clear the error label
+    }
 });
