ab7fdbca41056de6f324443df8ad336ab867ae91
OshandaB <oshanda893@gmail.com>
2024-01-25 15:02:09 +0530

    pie chart added

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,6 +1,56 @@
-$(document).ready(function () {
+$(window).on('load', function() {
     getTrainerCount();
+
+
+
 });
+var trainerCount;
+var memberCount;
+var workoutCount;
+var mealCount;
+var equipmentCount;
+function pieChart() {
+    const chartData = {
+        labels: ["Trainers", "Members", "MealPlans", "WorkOutPlans","Equipments"],
+        data: [trainerCount, memberCount, mealCount, workoutCount,equipmentCount],
+    };
+
+    const myChart = document.querySelector(".my-chart");
+    const ul = document.querySelector(".programming-stats .details ul");
+
+    new Chart(myChart, {
+        type: "doughnut",
+        data: {
+            labels: chartData.labels,
+            datasets: [
+                {
+                    label: "Count",
+                    data: chartData.data,
+                },
+            ],
+        },
+        options: {
+            borderWidth: 10,
+            borderRadius: 2,
+            hoverBorderWidth: 0,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        },
+    });
+
+    const populateUl = () => {
+        chartData.labels.forEach((l, i) => {
+            let li = document.createElement("li");
+            li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+            ul.appendChild(li);
+        });
+    };
+
+    populateUl();
+}
 
 function getTrainerCount(){
     $.ajax({
@@ -10,8 +60,10 @@ function getTrainerCount(){
         contentType: 'application/json',
         success: function (response) {
             trainerCount = response.data;
+            console.log(trainerCount)
             $("#trainersCount").text(trainerCount);
             getMemberCount();
+
         },
         error: function (jqXHR) {
             console.log(jqXHR.responseText);
@@ -51,6 +103,7 @@ function getWorkoutPlansCount(){
             }else{
                 $("#workoutCount").text(workoutCount);
             }
+
             getMealPlansCount();
         },
         error: function (jqXHR) {
@@ -74,9 +127,33 @@ function getMealPlansCount(){
             }else{
                 $("#mealCount").text(workoutCount);
             }
+            getEquipmentCount()
         },
         error: function (jqXHR) {
             console.log(jqXHR.responseText);
         }
     })
 }
+
+function getEquipmentCount(){
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/getEquipmentCount',
+        method: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+            equipmentCount = response.data;
+
+
+            pieChart();
+        },
+        error: function (jqXHR) {
+            console.log(jqXHR.responseText);
+        }
+    })
+}
+
+
+
+
+
